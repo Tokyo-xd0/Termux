edd40a5ffe98338148bcea70170c951fd292f363
Tokyo-xd0 <107883048+Tokyo-xd0@users.noreply.github.com>
2022-06-20 21:20:33 +0300

    Add files via upload

---
+++ Tokyo.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+
+pkg remove  game-repo
+pkg remove  science-repo
+clear
+pkg install figlet 
+clear
+figlet TOKYO
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+printf "\033[92m#        This Tool Crate BY       #\n\r"
+printf "\033[92m#             TOKYO               #\n\r"
+printf "\033[92m#        Telegram {i4m_Tokyo}     #\n\r"
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+sleep 3
+xdg-open https://t.me/Kurdish_Cracker_Official
+pkg update -y
+clear
+
+clear
+figlet TOKYO
+printf "\033[92m##########<<  TOKYO >>#############\n\r"
+printf "\033[91m#   Wait For install All Command  #\n\r"
+printf "\033[91m#         Kurdish Cracker         #\n\r"
+printf "\033[92m##########<<  TOKYO >>#############\n\r"
+sleep 5
+termux-setup-storage
+clear
+sleep 3
+figlet TOKYO
+printf "\033[93m##########<< TOKYO >>###############\n\r"
+printf "\033[91m#      Wait For Install python    #\n\r"
+printf "\033[92m#         Kurdish Cracker         #\n\r"
+printf "\033[93m##########<< TOKYO >>##############\n\r"
+sleep 4
+pkg install python -y
+clear
+printf "\033[94m#############<< TOKYO >>################\n\r"
+printf "\033[92m          Finsh Commamd Python         #\n\r"
+printf "\033[92m#           Kurdish Cracker            #\n\r"
+printf "\033[94m#############<< TOKYO >>################\n\r"
+clear
+figlet TOKYO
+printf "\033[93m##########<<  TOKYO >>##############\n\r"
+printf "\033[91m#       Wait For install Python2   #\n\r"
+printf "\033[92m#          Kurdish Cracker         #\n\r"
+printf "\033[93m##########<<  TOKYO >>##############\n\r"
+sleep 4
+pkg install python2 -y
+clear
+printf "\033[94m#############<< TOKYO >>################\n\r"
+printf "\033[92m#          Finsh Command Python2       #\n\r"
+printf "\033[92m#            Kurdish Cracker           #\n\r"
+printf "\033[94m#############<< TOKYO >>################\n\r"
+clear
+figlet TOKYO
+printf "\033[91m##########<<  TOKYO >>############\n\r"
+printf "\033[95m#       Wait For Install Git     #\n\r"
+printf "\033[92m#          Kurdish Cracker       #\n\r"
+printf "\033[91m##########<<  TOKYO >>############\n\r"
+sleep 4
+pkg install git -y
+clear
+printf "\033[94m#############<< TOKYO >>################\n\r"
+printf "\033[92m#           Finsh Commamd Git          #\n\r"
+printf "\033[92m#             Kurdish Cracker          #\n\r"
+printf "\033[94m#############<< TOKYO >>################\n\r"
+clear
+figlet TOKYO
+printf "\033[91m##########<< TOKYO >>##############\n\r"
+printf "\033[95m#         Wait For install Pip2   #\n\r"
+printf "\033[92m#           Kurdish Cracker       #\n\r"  
+printf "\033[91m##########<<  TOKYO >>#############\n\r"
+sleep 4
+pip2 install requests mechanize
+clear
+printf "\033[92m#############<< TOKYO >>################\n\r"
+printf "\033[95m#             Finsh Pip2               #\n\r"   
+printf "\033[95m#           Kurdish Cracker            #\n\r"
+printf "\033[92m#############<<SHELL TOKYO>>############\n\r"
+clear
+figlet TOKYO
+printf "\033[95m##########<< TOKYO >>##############\n\r"
+printf "\033[92m#        Wait For install Pip     #\n\r"
+printf "\033[92m#          Kurdish Cracker        #\n\r"
+printf "\033[95m##########<< TOKYO >>##############\n\r"
+sleep 4
+pip install requests  mechanize 
+clear
+printf "\033[96m#############<< TOKYO >>################\n\r"
+printf "\033[91m#              Finsh Pip               #\n\r"
+printf "\033[95m#            Kurdish Cracker           #\n\r"
+printf "\033[92m#############<< TOKYO >>################\n\r"
+clear
+figlet TOKYO
+printf "\033[94m##########<< TOKYO >>##############\n\r"
+printf "\033[93m#       Wait For install  bs4     #\n\r"
+printf "\033[93m#          Kurdish Cracker        #\n\r"
+printf "\033[94m##########<< TOKYO >>##############\n\r"
+sleep 4
+pip install bs4
+clear
+printf "\033[97m#############<< TOKYO >>################\n\r"
+printf "\033[92mm               Finsh bs4              #\n\r"
+printf "\033[92m#             Kurdish Cracker          #\n\r"
+printf "\033[97m#############<< TOKYO >>################\n\r"
+clear
+figlrt TOKYO
+printf "\033[92m##########< <TOKYO >>##############\n\r"
+printf "\033[97m#       Wait For install rich     #\n\r"
+printf "\033[97m#          Kurdish Cracker        #\n\r"
+printf "\033[92m##########<< TOKYO >>##############\n\r"
+sleep 4
+pip install rich
+clear
+printf "\033[93m#############<< TOKYO >>################\n\r"
+printf "\033[92m#             Finsh rich               #\n\r"
+printf "\033[92m#           Kurdish Cracker            #\n\r"
+printf "\033[93m#############<< TOKYO >>################\n\r"
+clear
+figlet TOKYO
+printf "\033[96m##########<< TOKYO >>##############\n\r"
+printf "\033[91m#        Wait For install Figlet  #\n\r"
+printf "\033[91m#           Kurdish Cracker       #\n\r"
+printf "\033[96m##########<< TOKYO >>#############\n\r"
+sleep 4
+pkg install figlet -y
+clear
+
+printf "\033[96m#############<< TOKYO >>################\n\r"
+printf "\033[92m#         Finsh Commamd Figlet         #\n\r"
+printf "\033[92m#           Kurdish Cracker            #\n\r"
+printf "\033[96m#############<< TOKYO >>################\n\r"
+clear
+figlet TOKYO
+
+printf "\033[92m##########<< TOKYO >>##############\n\r"
+printf "\033[95m#       install all Commamd       #\n\r"
+printf "\033[95m#         Kurdish Cracker         #\n\r"
+printf "\033[92m##########<< TOKYO >>##############\n\r"
+sleep 4
+
+clear
+figlet TELEGRAM
+xdg-open https//t.me/Kurdish_Cracker_Official
+sleep 10
+clear
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+printf "\033[92m#        This Tool Crate BY       #\n\r"
+printf "\033[92m#               TOKYO             #\n\r"
+printf "\033[92m#        Telegram {i4m_Tokyo}     #\n\r"
+printf "\033[91m#############<<TOKYO>>#############\n\r"
+sleep 4
+figlet Bay
+
